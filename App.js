66c6fd4e3b874608c63def95b26f4b2e42c0f8f9
Heorgii <rushchakheorgii@gmail.com>
2023-03-04 18:12:12 +0300

    update

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
-import RegistrationScreen from './Screens/RegistrationScreen/RegistrationScreen';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+// import RegistrationScreen from './Screens/RegistrationScreen/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen/LoginScreen';
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        style={styles.image}
-        source={require("./Screens/image/bgImage.jpg")}>
-        <RegistrationScreen></RegistrationScreen>
-
+        source={require("./Screens/image/bgImage.jpg")}
+        style={styles.image}>
+        {/* <RegistrationScreen></RegistrationScreen> */}
+        <LoginScreen />
       </ImageBackground>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -19,15 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-
   image: {
     flex: 1,
-    // resizeMode: 'cover',
-    width: 500,
-    justifyContent: 'center'
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
   },
 });
---
+++ Screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView } from "react-native";
+
+export default RegistrationScreen = () => {
+    const [isActiveKeyBoard, setIsActiveKeyBoard] = useState(false);
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <View style={{ ...styles.form, padding: isActiveKeyBoard ? 32 : 1 }}>
+
+                <Text style={styles.title}>Log in</Text>
+
+                <TextInput style={{ ...styles.input, marginBottom: 16}} placeholder="Email" onFocus={() => setIsActiveKeyBoard(false)} />
+                <TextInput style={styles.input} secureTextEntry={true} placeholder="Password" onFocus={() => setIsActiveKeyBoard(false)} />
+
+                <TouchableOpacity activeOpacity={0.5}
+                    style={styles.form_button} >
+                    <Text style={styles.form_button__text}>Log in</Text>
+                </TouchableOpacity>
+                <Text style={styles.form_text}>Don't have an account? Register</Text>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    form: {
+        backgroundColor: '#FFFFFF',
+        height: 549,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+    },
+    title: {
+        marginTop: 32,
+        marginBottom: 32,
+        fontFamily: 'Roboto',
+        fontWeight: 500,
+        fontSize: 30,
+        lineHeight: 35,
+        textAlign: 'center',
+        letterSpacing: 0.01,
+        color: '#212121',
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: '#E8E8E8',
+        backgroundColor: '#F6F6F6',
+        height: 50,
+        paddingLeft: 16,
+        marginHorizontal: 16,
+    },
+    form_button: {
+        ...Platform.select({
+            ios: {
+                backgroundColor: '#FF6C00',
+            },
+            android: {
+                backgroundColor: '#FF6C33',
+            }
+        }),
+        // display: 'none',
+        height: 51,
+        borderRadius: 100,
+        marginHorizontal: 16,
+        marginTop: 43,
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    form_button__text: {
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+        fontSize: 16,
+        lineHeight: 19,
+        textAlign: 'center',
+        color: '#FFFFFF',
+    },
+    form_text: {
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+        fontSize: 16,
+        lineHeight: 19,
+        textAlign: 'center',
+        color: '#1B4371',
+        marginTop: 16,
+    },
+});
